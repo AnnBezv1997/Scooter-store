9259fbc884c64dca674eed508cb377d5cd0ab39b
AnnBezv1997 <73289700+AnnBezv1997@users.noreply.github.com>
2021-05-19 10:09:32 +0300

    Delete Order directory

--- Order/src/main/java/com/ncedu/scooter/order/entity/OrderStatus.java
+++
@@ -1,6 +0,0 @@
-package com.ncedu.scooter.order.entity;
-
-public enum OrderStatus {
-    DELIVERED,
-    CANCELED
-}
--- Order/src/main/java/com/ncedu/scooter/order/exeption/ExceptionMessage.java
+++
@@ -1,6 +0,0 @@
-package com.ncedu.scooter.order.exeption;
-
-public class ExceptionMessage {
-    public static final String PRODUCT_QUANTITY_ERROR = "The quantity of the product is higher than available in stock.";
-
-}
