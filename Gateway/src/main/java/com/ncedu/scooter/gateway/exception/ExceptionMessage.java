07581505084250d1c8b81b37f7c6e5b5aea80cd8
AnnBezv1997 <73289700+AnnBezv1997@users.noreply.github.com>
2021-05-19 10:09:19 +0300

    Delete Gateway directory

--- Gateway/src/main/java/com/ncedu/scooter/gateway/exception/ExceptionMessage.java
+++
@@ -1,7 +0,0 @@
-package com.ncedu.scooter.gateway.exception;
-
-public class ExceptionMessage {
-    public static final String USER_NOT_FOUND = "The user does not exist.";
-    public static final String PASSWORD_INCORRECT = "The password is incorrect.";
-    public static final String ERROR_SAVE = "Save error.";
-}
